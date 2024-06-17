c360c5d7949bc0e686679be347407c63ff042426
anonymousboy67 <aashishad67@gmail.com>
2024-06-17 08:42:23 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Generator from './components/Generator'
+import Hero from './components/Hero'
+import Workout from './components/Workout'
+
+function App() {
+ 
+
+  return (
+   <main className='min-h-screen flex flex-col bg-gradient-to-r
+    from-slate-800 text-white text-sm sm:text-base'>
+   <Generator/>
+   <Hero/>
+   <Workout/>
+   
+   </main>
+  )
+}
+
+export default App
